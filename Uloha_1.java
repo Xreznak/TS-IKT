361a0e84f4cadc61fa4274ff1cdee9dd9fff4b32
Xreznak <Xreznak@stuba.sk>
2023-10-12 10:50:55 +0200

    Cvicenie_4

--- Uloha_1.java
+++ Uloha_1.java
@@ -1,2 +1,48 @@
+import java.util.Scanner;
+
 public class Uloha_1 {
+
+    public static void zretazenie() {
+        String retazec_1 = ("Ahoj Jano");
+        String retazec_2 = (" ako sa mas?");
+        String retazec_3 = retazec_1 + retazec_2;
+        System.out.println("Spojený retazec: "+ retazec_3);
+    }
+
+    public static void dlzka() {
+        String retazec_1 = ("Ahoj Jano");
+        int dlzka = retazec_1.length();
+        System.out.println("Dĺžka reťazca je: "+ dlzka);
+    }
+
+    public static void porovnanie() {
+        String retazec_1 = ("Ahoj Jano");
+        String retazec_2 = ("Ahoj Jano");
+        if (retazec_1.equals(retazec_2))
+        {
+            System.out.println("Reťazce sú rovnaké");
+        }
+        else
+        {
+            System.out.println("Reťazce nie sú rovnaké");
+        }
+    }
+
+    public static void hladaj_podretazec() {
+        String zadany_retazec = ("Ahoj Jano");
+        String hladany_retazec = ("Jano");
+
+        if(zadany_retazec.contains(hladany_retazec))
+        {
+            System.out.println(hladany_retazec+" sa nachádza v zadanom reťazci.");
+        }else
+        {
+            System.out.println(hladany_retazec+" sa nenachádza v zadanom reťazci.");
+        }
+    }
+
+    public static void velke_pismena() {
+        String retazec = ("Ahoj Jano");
+        System.out.println(retazec.toUpperCase());
+    }
 }
--- main.java
+++ main.java
@@ -1,18 +1,11 @@
 public class main {
     public static void main(String[] args) {
 
-        Uloha_1.mrgString();
-        Uloha_1.lngtString();
-        Uloha_1.iseqlString();
-        Uloha_1.searchString();
-        Uloha_1.toupString();
-        Uloha_2.tolowString();
-        Uloha_2.spaceRemove();
-        Uloha_2.strToWord();
-        Uloha_2.rplcString();
-        Uloha_2.palindrom();
-
-        Zamestnanec z01 = new Zamestnanec("Michal",20,0001);
+        Uloha_1.zretazenie();
+        Uloha_1.dlzka();
+        Uloha_1.porovnanie();
+        Uloha_1.hladaj_podretazec();
+        Uloha_1.velke_pismena();
 
     }
 }
