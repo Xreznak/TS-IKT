3328546b6d895d685a8b5e4165340eac127126e3
Xreznak <Xreznak@stuba.sk>
2023-10-12 10:50:55 +0200

    Cvicenie_4

--- Uloha_1.java
+++ Uloha_1.java
@@ -1,2 +1,70 @@
+import java.util.Scanner;
+
 public class Uloha_1 {
+
+    public static void zretazenie() {
+        String retazec_1 = ("Ahoj Jano");
+        String retazec_2 = (" ako sa mas?");
+        String retazec_3 = retazec_1 + retazec_2;
+
+        System.out.println("\nzretazenie");
+        System.out.println("Reťazec 1:" + retazec_1);
+        System.out.println("Reťazec 2:" + retazec_2);
+
+        System.out.println("Spojený retazec: "+ retazec_3);
+    }
+
+    public static void dlzka() {
+        String retazec_1 = ("Ahoj Jano");
+
+        System.out.println("\ndlzka");
+        System.out.println("Reťazec:" + retazec_1);
+
+        int dlzka = retazec_1.length();
+        System.out.println("Dĺžka reťazca je: "+ dlzka);
+    }
+
+    public static void porovnanie() {
+        String retazec_1 = ("Ahoj Jano");
+        String retazec_2 = ("Ahoj Jano");
+
+        System.out.println("\nporovnanie");
+        System.out.println("Reťazec 1:" + retazec_1);
+        System.out.println("Reťazec 2:" + retazec_2);
+
+        if (retazec_1.equals(retazec_2))
+        {
+            System.out.println("Reťazce sú rovnaké");
+        }
+        else
+        {
+            System.out.println("Reťazce nie sú rovnaké");
+        }
+    }
+
+    public static void hladaj_podretazec() {
+        String zadany_retazec = ("Ahoj Jano");
+        String hladany_retazec = ("Jano");
+
+        System.out.println("\nhladaj_podretazec");
+        System.out.println("Zadaný reťazec:" + zadany_retazec);
+        System.out.println("Hladaný reťazec:" + hladany_retazec);
+
+        if(zadany_retazec.contains(hladany_retazec))
+        {
+            System.out.println(hladany_retazec+" sa nachádza v zadanom reťazci.");
+        }else
+        {
+            System.out.println(hladany_retazec+" sa nenachádza v zadanom reťazci.");
+        }
+    }
+
+    public static void velke_pismena() {
+        String retazec = ("Ahoj Jano");
+
+        System.out.println("\nvelke_pismena");
+
+        System.out.println("Zadaný reťazec:" + retazec);
+        System.out.println("CAPSLOCK reťazec:" + retazec.toUpperCase());
+    }
 }
--- Uloha_2.java
+++ Uloha_2.java
@@ -1,2 +1,71 @@
+import java.util.Scanner;
+
 public class Uloha_2 {
+    public static void male_pismena() {
+        String retazec = ("Ahoj Jano");
+
+        System.out.println("\nmale_pismena");
+        System.out.println("Zadaný reťazec:" + retazec);
+
+        System.out.println("Reťazec s malými písmenami:" + retazec.toLowerCase());
+    }
+
+    public static void bez_medzier() {
+        String retazec = ("Ahoj Jano");
+
+        System.out.println("\nbez_medzier");
+        System.out.println("Zadaný reťazec:" + retazec);
+
+        String bez_medzier = retazec.replaceAll(" ","");
+        System.out.println("Reťazec bez medzier: "+ bez_medzier);
+    }
+
+    public static void rozdel_retazec() {
+        String retazec = ("Ahoj Jano");
+
+        System.out.println("\nrozdel_retazec");
+        System.out.println("Zadaný reťazec:" + retazec);
+
+
+        String[] rozdel = retazec.split(" ");
+
+        System.out.println("Slová z reťazca:");
+        for(String retazec_rozdeleny : rozdel)
+        {
+            System.out.println(retazec_rozdeleny);
+        }
+    }
+
+    public static void zamenenie_znaku() {
+        String retazec_1 = ("Ahoj Jano");
+        String znak_povodny = ("J");
+        String znak_zmeneny = ("Z");
+
+        System.out.println("\nrozdel_retazec");
+        System.out.println("Zadaný reťazec:" + retazec_1);
+        System.out.println("Pôvodný znak:" + znak_povodny);
+        System.out.println("Zmenený znak:" + znak_zmeneny);
+
+        String retazec_2 = retazec_1.replaceAll(znak_povodny,znak_zmeneny);
+        System.out.println("Zmenený reťazec: "+ retazec_2);
+    }
+
+    public static void palindrom() {
+
+        String retazec = ("Ahoj johA");
+        String retazec_z_predu = retazec.replaceAll(" ","").toLowerCase();
+        String retazec_zo_zadu = new StringBuilder(retazec_z_predu).reverse().toString() ;
+
+        System.out.println("\npalindrom");
+        System.out.println("Zadaný reťazec:" + retazec);
+
+        if(retazec_z_predu.equals(retazec_zo_zadu))
+        {
+            System.out.println("Zadaný reťazec je palindróm");
+        }
+        else
+        {
+            System.out.println("Zadaný reťazec nie je palindróm");
+        }
+    }
 }
